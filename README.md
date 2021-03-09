8cee67e57d07d48691855a1267940c5f89988bc6
Alex Crocker <hello@crock.dev>
2021-03-09 18:45:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Disboard Scraper
+
+Source your brand new Discord server list with thousands of real Discord servers!
+
+Data is scraped and formatted like this...
+```json
+[
+{
+    "id": "579772930607808537",
+    "name": "\n\nAmong Us \n",
+    "reviewCount": "\n( 20 reviews )\n",
+    "iconUrl": "https://cdn.discordapp.com/icons/579772930607808537/a_e1cba5ab209770d3887b6fb310a8d756.jpg",
+    "membersOnline": "87387",
+    "tags": ["roleplay", "mobile", "murder-mystery", "among-us", "amongus"],
+    "description": "\nAmong Us is a free to play murder mystery game. There are two roles: crewmate and imposter. The crewmates' jobs is to either complete all their tasks or to eject the imposter. The Imposter's job is to kill all the crewmates. Join our Among Us Discord Server to find games with others and meet new people! "
+  }
+]
+```
---
+++ main.py
@@ -0,0 +1,137 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+import os
+
+servers = []
+
+with open('sites.json', 'r') as fp:
+    sites = json.load(fp)
+    if len(sites) >= 1:
+        print(f'Total Sites: {len(sites)}')
+
+    for site in sites:
+        print(site['name'])
+        session = requests.Session()
+        url = site['directoryIndexUrl'].replace('BASE_URL', site['baseUrl'])
+        if not os.path.exists(f"{site['name']}.html"):
+            response = session.get(url)
+            with open(f"{site['name']}.html", "w") as fx:
+                fx.write(response.text)
+        with open(f"{site['name']}.html", "r") as fx:
+            html = fx.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            numPagesText = soup.find(class_="listing-summary").get_text() # Showing 1 - 24 of 926745 servers
+            print(numPagesText)
+            matches = re.search(r"Showing\s+(\d+)\s+-\s+(\d+)\s+of\s+(\d+)\s+servers", numPagesText).groups(0)
+            if len(matches) == 3:
+                perPage = 24
+                totalServers = int(matches[2])
+                numPages = totalServers / perPage
+                print(f"Total Pages: {round(numPages)}")
+
+                rawServers = soup.find(class_="listing").find_all(class_="column")
+                for server in rawServers:
+                    reviewCountElem = server.find(class_="review-count")
+                    reviewCount = ""
+                    if reviewCountElem:
+                        reviewCount = reviewCountElem.get_text()
+
+                    iconUrl = server.find(class_="server-icon").img['src']
+
+                    name = server.find(class_="server-name").get_text()
+
+                    membersOnline = server.find(class_="server-online").get_text()
+
+                    tags = []
+                    rawTagsContainer = server.find(class_="server-tags")
+                    if (rawTagsContainer):
+                        rawTags = rawTagsContainer.find_all(class_="tag")
+                        for tag in rawTags:
+                            tags.append(tag['title'])
+
+                    serverDescElem = server.find(class_="server-description")
+                    serverDescription = ""
+                    if serverDescElem:
+                        serverDescription = serverDescElem.get_text()
+
+                    serverId = server.div['class'][1].replace('server-', '')
+                    
+                    servers.append({
+                        "id": serverId,
+                        "name": name,
+                        "reviewCount": reviewCount,
+                        "iconUrl": iconUrl,
+                        "membersOnline": membersOnline,
+                        "tags": tags,
+                        "description": serverDescription,
+                    })
+
+                with open(f"{site['name']}-1.json", "w") as fx:
+                    fx.write(json.dumps(servers))
+                
+                for page in range(2, round(numPages)):
+                    pageServers = []
+                    url = site['otherDirectoryPagesUrl'].replace('BASE_URL', site['baseUrl']) % page
+                    if not os.path.exists(f"{site['name']}-{page}.html"):
+                        response = session.get(url)
+                        with open(f"{site['name']}-{page}.html", "w") as fx:
+                            fx.write(response.text)
+                    with open(f"{site['name']}-{page}.html", "r") as fx:
+                        html = fx.read()
+                        soup = BeautifulSoup(html, 'html.parser')
+                        numPagesText = soup.find(class_="listing-summary").get_text() # Showing 1 - 24 of 926745 servers
+                        print(numPagesText)
+                        matches = re.search(r"Showing\s+(\d+)\s+-\s+(\d+)\s+of\s+(\d+)\s+servers", numPagesText).groups(0)
+                        if len(matches) == 3:
+                            totalServers = int(matches[2])
+                            numPages = totalServers / perPage
+                            print(f"Pages Remaining: {round(numPages) - page}")
+
+                            rawServers = soup.find(class_="listing").find_all(class_="column")
+                            for server in rawServers:
+                                reviewCountElem = server.find(class_="review-count")
+                                reviewCount = ""
+                                if reviewCountElem:
+                                    reviewCount = reviewCountElem.get_text()
+
+                                iconUrl = server.find(class_="server-icon").img['src']
+
+                                name = server.find(class_="server-name").get_text()
+
+                                membersOnline = server.find(class_="server-online").get_text()
+
+                                tags = []
+                                rawTagsContainer = server.find(class_="server-tags")
+                                if (rawTagsContainer):
+                                    rawTags = rawTagsContainer.find_all(class_="tag")
+                                    for tag in rawTags:
+                                        tags.append(tag['title'])
+
+                                serverDescElem = server.find(class_="server-description")
+                                serverDescription = ""
+                                if serverDescElem:
+                                    serverDescription = serverDescElem.get_text()
+
+                                serverId = server.div['class'][1].replace('server-', '')
+                                
+                                pageServers.append({
+                                    "id": serverId,
+                                    "name": name,
+                                    "reviewCount": reviewCount,
+                                    "iconUrl": iconUrl,
+                                    "membersOnline": membersOnline,
+                                    "tags": tags,
+                                    "description": serverDescription,
+                                })
+
+                    with open(f"{site['name']}-{page}.json", "w") as fx:
+                        fx.write(json.dumps(pageServers))   
+                        
+                    
+
+
+
+
+        
